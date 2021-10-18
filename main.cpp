4029e0d1937a4eae61aae087ddb5db220a61dd2c
justNeto <continental.lelgg.2b@gmail.com>
2021-10-17 21:58:07 -0500

    The class now saves the complete error message of server failure

--- main.cpp
+++ main.cpp
@@ -1,43 +1,71 @@
 #include <iostream>
 #include <fstream>	// Leer el archivo
-#include <sstream>	// tokenizar el string de cada registro
+#include <string>
+#include <sstream>	// tokenizar el string de cada myTextistro
 #include <random>
-
-#include "bst.h"
-#include "Server.h"
 
 using namespace std;
 
 int main()
 {
-	// Creating Binary Tree
-	BST<LinkedList<Server>*> mainTree = BST<LinkedList<Server>*> ();
-
 	// Create a text string, which is used to output the text file
 	string mes, hora, ip, errorLog;
 	int dia;
-	string myText;
+	string myText; // Auxiliar text for each line
 
 	// Read from the text file
-	ifstream MyReadFile("bitacora.txt");
-	int llSize = 16807;
-	int randomSize, randomInfected;
-	LinkedList<Server>* auxList = new LinkedList<Server> (NULL);
-	Server auxServer;
+	ifstream MyReadFile("bitacora.txt"); //  Oct 9 10:32:24 423.2.230.77:6166 Failed password for illegal user guest
 
-	randomSize = (rand() % 200) + 1;
 
 	// Use a while loop together with the getline() function to read the file line by line
 	while (getline (MyReadFile, myText))
 	{
-		istringstream tokens(myText);
-		tokens >> mes;
-		tokens >> dia;
-		tokens >> hora;
-		tokens >> ip;
-		tokens >> errorLog;
-
-		int randomInfected = (rand() % 1000) + 1;
+		int aux = 1, aux3 = 1; // aux determines when a word is completed, aux3 for last str
+		string aux2 = ""; // string aux2 is where data will be saved for later
+		for (int i = 0; i < myText.length(); i++)
+		{
+			if (myText[i] == ' ' && aux3 <= 4) // For the first 4 categories
+			{
+				aux++;
+				if (aux == 2 && aux3 == 1)
+				{
+					aux = 1;
+					mes = aux2;
+					aux2 = "";
+					aux3++;
+				}
+				else if (aux == 2 && aux3 == 2)
+				{
+					aux = 1;
+					dia = stoi(aux2);
+					aux2 = "";
+					aux3++;
+				}
+				else if (aux == 2 && aux3 == 3)
+				{
+					aux = 1;
+					hora = aux2;
+					aux2 = "";
+					aux3++;
+				}
+				else
+				{
+					aux = 1;
+					ip = aux2;
+					aux2 = "";
+					aux3++;
+				}
+			}
+			else if (aux3 > 4)
+			{
+				aux2 += myText[i];
+			}
+			else
+			{
+				aux2 += myText[i];
+			}
+			errorLog = aux2;
+		}
 
 		if (randomSize > 0)
 		{
@@ -64,4 +92,4 @@ int main()
 
 	// Close the file
 	MyReadFile.close();
-};
+}
