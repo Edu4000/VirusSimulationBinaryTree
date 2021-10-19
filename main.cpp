0d17f1535b6f331f64751c3ee46479967203a272
justNeto <continental.lelgg.2b@gmail.com>
2021-10-19 17:44:43 -0500

    Update main.cpp
    Menu WIP

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 #include <string>
 #include <sstream>	// tokenizar el string de cada myTextistro
 #include <random>
-
 #include "bst.h"
 #include "Server.h"
 
 using namespace std;
 
-int main()
+BST makeTree()
 {
-	// Creating Binary Tree
+	// Tree is created with BST and LinkedList and Server Class
 	BST<LinkedList<Server>*> mainTree = BST<LinkedList<Server>*> ();
 
 	// Create a text string, which is used to output the text file
@@ -22,14 +21,14 @@ int main()
 	// Read from the text file
 	ifstream MyReadFile("bitacora.txt"); //  Oct 9 10:32:24 423.2.230.77:6166 Failed password for illegal user guest
 
-	int randomSize, randomInfected;
+	int randomInfected;
 	LinkedList<Server>* auxList = new LinkedList<Server> (NULL);
 	Server auxServer;
+	int randomSize = (rand() % 200) + 1;
 
 	// Use a while loop together with the getline() function to read the file line by line
 	while (getline (MyReadFile, myText))
 	{
-
 		int aux = 1, aux3 = 1; // aux determines when a word is completed, aux3 for last str
 		string aux2 = ""; // string aux2 is where data will be saved for later
 		for (int i = 0; i < myText.length(); i++)
@@ -94,28 +93,51 @@ int main()
 		}
 	}
 	mainTree.insert(auxList);
-	cout << "Listo!!" << endl;
-	cout << mainTree.lenght() << endl;
-
-	cout << mainTree.first()->size() << endl;
-
-	cout << mainTree.is_full() << endl;
-
-	cout << mainTree.leaf_count() << endl;
-
-	cout << mainTree.is_perfect() << endl;
-
-	// Binary tree to test the functions
-	BST<int> testTree = BST<int> ();
-	testTree.insert(10);
-	testTree.insert(5);
-	testTree.insert(15);
-
-	// arbolPruebas.inOrden();
-	cout << testTree.is_full() << endl;
-	cout << testTree.leaf_count() << endl;
-	cout << testTree.is_perfect() << endl;
-
 	// Close the file
 	MyReadFile.close();
+
+	return mainTree;
+}
+
+
+int main()
+{
+	while (true)
+	{
+		int choice; bool fileLoaded = false;
+		cout << "| ==================== Menu  ==================== |" << endl;
+		cout << "1. Cargar el archivo" << endl;
+		cout << "2. Imprimir arbol por nivel" << endl;
+		cout << "3. Imprimir arbol por pre-orden" << endl;
+		cout << "4. Imprimir arbol por post-orden" << endl;
+		cout << "5. Imprimir arbol por in-orden" << endl;
+		cout << "6. Buscar si existe servidor por ip" << endl;
+		cout << "7. Imprimir porcentaje de servidores infectados / no infectados" << endl;
+		cout << "9. Imprimir resultados de los metodos de BST" << endl;
+
+		if (choice != 0 && fileLoaded == false)
+		{
+			continue;
+		}
+
+		switch (choice)
+		{
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			default:
+				break;
+		}
+	}
+
+
+	// Creating Binary Tree
+	BST<LinkedList<Server>*> mainTree = makeTree();
 }
