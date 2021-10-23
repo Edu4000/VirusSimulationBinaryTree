300029d59a7426dfdc5f7d540ec2de1b701663fb
Edu4000 <edag4000@gmail.com>
2021-10-23 15:39:34 -0500

    LinkedList.h Documentation
    Fully documented all the classes and methods used in linked list.

--- LinkedList.h
+++ LinkedList.h
@@ -1,107 +1,156 @@
+/**
+ * @file LinkedList.h
+ * @author TheDevs
+ * @brief LinkedList class used in VirusSimulation
+ * @version 0.1
+ * @date 2021-10-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief Node class that is used as pointer in LinkedList
+ * 
+ * @tparam var 
+ */
 template<typename var>
-class Node
-{
+class Node{
 	private:
-		var data; // O(1)
-		Node<var>* nextPtr; // O(n)
-		Node<var>* prevPtr; // O(n)
-		template<typename T>
-		friend class LinkedList; // O(n)
+	var data; // O(1)
+	Node<var>* nextPtr; // O(1)
+	Node<var>* prevPtr; // O(1)
+	template<typename T>
+	friend class LinkedList; // O(1)
 
 	public:
-		Node(var data) // O(1)
-		{
-			this->data = data; // O(1)
-			this->nextPtr = NULL; // O(1)
-			this->prevPtr = NULL; // O(1)
-		}
+	/**
+	 * @brief Construct a new Node object. O(1)
+	 * 
+	 * @param data Data type var to be stored in Node
+	 */
+	Node(var data){ // O(1)
+		this->data = data; // O(1)
+		this->nextPtr = NULL; // O(1)
+		this->prevPtr = NULL; // O(1)
+	}
 
-		friend ostream& operator <<(ostream & salida, const Node & node)
-		{
-			salida << node.data;
-			return salida;
-		}
+	/**
+	 * @brief Overload operator of <<, used to print Node.data. O(1)
+	 * 
+	 * @param salida Out stream variable
+	 * @param node The node object
+	 * @return ostream& 
+	 */
+	friend ostream& operator <<(ostream & salida, const Node & node){ //O(1)
+		salida << node.data;
+		return salida;
+	}
 };
 
+/**
+ * @brief LinkedList class that is used in VirusSimulation
+ * 
+ * @tparam var 
+ */
 template<typename var>
-class LinkedList
-{
+class LinkedList{
 	private:
-	Node<var>* header; // O(n)
-	Node<var>* last; // O(n)
+	Node<var>* header; // O(1)
+	Node<var>* last; // O(1)
 
-	/* bool containsRecursivo(var data, Node<var> * ptr) // O(n) */
-	/* { */
-	/* 	if (ptr == NULL) // O(1) */
-	/* 	{ */
-	/* 		return false; // O(1) */
-	/* 	} */
-	/* 	else if(data == ptr->data) // O(1) */
-	/* 	{ */
-	/* 		return true; // O(1) */
-	/* 	} */
-	/* 	return containsRecursivo(data, ptr->nextPtr); // O(n) */
-	/* } */
-
-	int lastIndexOfRecursivo(int index, var data, Node<var> * ptr) // O(n)
-	{
-		if (ptr == NULL) // O(1)
-		{
+	/**
+	 * @brief Returns index of node where data is located in the list. Goes from last(-1) to header(-n). O(n)
+	 * 
+	 * @param index Default value of -1
+	 * @param data Data to be found in LinkedList
+	 * @param ptr Pointer to node whith data to be compared
+	 * @return int - Index of data in Linked list
+	 */
+	int lastIndexOfRecursivo(int index, var data, Node<var> * ptr){ // O(n)
+		if (ptr == NULL){
 			return NULL; // O(1)
 		}
-		else if(data == ptr->data) // O(1)
-		{
+		else if(data == ptr->data){
 			return index; // O(1)
 		}
 		return lastIndexOfRecursivo(index - 1, data, ptr->prevPtr); // O(n)
 	}
 
-	int indexOfRecursivo(int index, var data, Node<var> * ptr) // O(n)
-	{
-		if (ptr == NULL) // O(1)
-		{
+	/**
+	 * @brief Returns index where data is located in the list. Goes from header(0) to last(n). O(n)
+	 * 
+	 * @param index Default value of 0
+	 * @param data Data to be found in LinkedList
+	 * @param ptr Pointer to node whith data to be compared
+	 * @return int - Index of data in Linked list
+	 */
+	int indexOfRecursivo(int index, var data, Node<var> * ptr){ // O(n)
+		if (ptr == NULL){
 			return NULL; // O(1)
 		}
-		else if(data == ptr->data) // O(1)
-		{
+		else if(data == ptr->data){
 			return index; // O(1)
 		}
 		return indexOfRecursivo(index + 1, data, ptr->nextPtr); // O(n)
 	}
 
-	bool operator > (const LinkedList<var>& linkedL) // O(n)
-	{
+	/**
+	 * @brief Operator overload >, compares length of the LinkedList to another LinkedList. O(n)
+	 * 
+	 * @param linkedL LinkedList to be compared
+	 * @return TRUE if THIS LinkedList is bigger than linkdeL, else return FALSE
+	 */
+	bool operator > (const LinkedList<var>& linkedL){ // O(n)
 		return this->size() > linkedL.size(); // O(n)
 	}
 
-	bool operator < (const LinkedList<var>& linkedL) // O(n)
-	{
+	/**
+	 * @brief Operator overload <, compares length of the LinkedList to another LinkedList. O(n)
+	 * 
+	 * @param linkedL LinkedList to be compared
+	 * @return TRUE if THIS LinkedList is smaller than linkdeL, else return FALSE
+	 */
+	bool operator < (const LinkedList<var>& linkedL){ // O(n)
 		return this->size() < linkedL.size(); // O(n)
 	}
 
 	public:
-	// Constructor
+	/**
+	 * @brief Construct a new Linked List object, the first pointer will be header and last. O(1)
+	 * 
+	 * @param header Pointer to Node<var> which is the header
+	 */
 	LinkedList(Node<var>* header) { // O(1)
 		this->header = header; // O(1)
 		this->last = header; // O(1)
 	}
 
-	// Add First
-	void addFirst(var data) {
+	/**
+	 * @brief Add new node with data to the first position (header). O(1)
+	 * 
+	 * @param data Data to be added to the LinkedList
+	 */
+	void addFirst(var data){
 		Node<var>* new_node = new Node<var>(data);
-		if (header == NULL) {	// Linked list is empty
+		if (header == NULL){ // Linked list is empty
 			header = last = new_node;
-		} else {	// It points to one node
+		}
+		else{ // It points to one node
 			new_node->nextPtr = header;
 			header->prevPtr = new_node;
 			header = new_node;
 		}
 	}
 
-	// Add Last							O(n)
+	/**
+	 * @brief Add new node with data to the last position (last). O(1)
+	 * 
+	 * @param data Data to be added to the LinkedList
+	 */
 	void addLast(var data) {
 		Node<var>* curr = header; // Current (temporal) pointer for iteration starts in the header pointer
 		Node<var>* new_node = new Node<var>(data); // pointer to a new Node with data is created
@@ -114,25 +163,22 @@ class LinkedList
 		}
 	}
 
-	// Is Empty							O(1)
+	/**
+	 * @brief Checks if the LinkedList is empty. O(1)
+	 * 
+	 * @return TRUE if LinkedList is empty, else FALSE
+	 */
 	bool is_empty(){
 		return header == NULL ? true : false;	// O(1)
 	}
 
-	// Contains							O(n)
-	/* bool contains(var data) { */
-	/* 	Node<var>* ptr = this->header;	// O(1) */
-	/* 	while (ptr != NULL) {			// O(n) */
-	/* 		if (ptr->data == data) {	// O(1) */
-	/* 			return true;			// O(1) */
-	/* 		} */
-	/* 		ptr = ptr->nextPtr;			// O(n) */
-	/* 	} */
-	/* 	return false;					// O(1) */
-	/* } */
-
-	// Get								O(n)
-	var get(int index) {
+	/**
+	 * @brief Gets the data stored in the index indicated. O(n)
+	 * 
+	 * @param index Index where data is stored
+	 * @return var - Data stored in the index position
+	 */
+	var get(int index){
 		try {						// O(1)
 			if (index < 0) {		// O(1)
 				throw (index);		// O(1)
@@ -158,7 +204,11 @@ class LinkedList
 		}
 	}
 
-	// Remove Front  					O(1)
+	/**
+	 * @brief Gets data stored in header and erases the node that contains it. O(1)
+	 * 
+	 * @return var - Data stored in header
+	 */
 	var remove_front(){
 		try {                        // O(1)
 			if (header == last) {	 // O(1)
@@ -183,8 +233,12 @@ class LinkedList
 		}
 	}
 
-	// Remove Last						O(n)
-	var remove_last() {
+	/**
+	 * @brief Gets data stored in last and erases the node that contains it. O(n)
+	 * 
+	 * @return var - Data stored in last
+	 */
+	var remove_last(){
 		Node<var>* ptr = header;			// O(1)
 		Node<var>* ptr2 = header->nextPtr;	// O(1)
 		while(ptr2->nextPtr != NULL) {		// O(n)
@@ -196,8 +250,13 @@ class LinkedList
 		return ptr2->data;
 	}
 
-	// Add At							O(n)
-	void add_at(var data, int index) {
+	/**
+	 * @brief Adding data in desired index by inmtroducing new node in LinkedList. O(n)
+	 * 
+	 * @param data Data to be stored
+	 * @param index Position where data will be stored in LinkedList
+	 */
+	void add_at(var data, int index){
 		try {						// O(1)
 			if (index < 0) {		// O(1)
 				throw (index);		// O(1)
@@ -233,7 +292,11 @@ class LinkedList
 		}
 	}
 
-	// Size
+	/**
+	 * @brief Checks the size of the LinkedList. O(n)
+	 * 
+	 * @return int - Size of LinkedList
+	 */
 	int size() {
 		Node<var>* curr = header;
 		int i = 0;
@@ -244,7 +307,10 @@ class LinkedList
 		return i;
 	}
 
-	// Print
+	/**
+	 * @brief Prints all the nodes in the LinkedList, from header to last. O(n)
+	 * 
+	 */
 	void print() {
 		Node<var>* ptr = header;
 		while(ptr != NULL) {
@@ -254,7 +320,10 @@ class LinkedList
 		cout << endl;
 	}
 
-	// Reverse Print
+	/**
+	 * @brief Prints all the nodes in the LinkedList, from last to header. O(n)
+	 * 
+	 */
 	void reverse() {
 		Node<var>* ptr = last;
 		while(ptr != NULL) {
@@ -264,7 +333,11 @@ class LinkedList
 		cout << endl;
 	}
 
-	// Get First
+	/**
+	 * @brief Get the data of the first node (header). O(1)
+	 * 
+	 * @return var - Data stored in header
+	 */
 	var get_first() {
 		if (header != NULL) {
 			return header->data;
@@ -273,7 +346,11 @@ class LinkedList
 		}
 	}
 
-	// Get Last
+	/**
+	 * @brief Get the data of the last node (last). O(1)
+	 * 
+	 * @return var - Data stored in last
+	 */
 	var get_last() {
 		if (header != NULL) {
 			return last->data;
@@ -282,7 +359,11 @@ class LinkedList
 		}
 	}
 
-	// Peek First
+	/**
+	 * @brief Get the data of the first node (header). O(1)
+	 * 
+	 * @return var - Data stored in header
+	 */
 	var peekFirst() {
 		if (header != NULL) {
 			return header->data;
@@ -291,7 +372,11 @@ class LinkedList
 		}
 	}
 
-	// Peek Last
+	/**
+	 * @brief Get the data of the last node (last). O(1)
+	 * 
+	 * @return var - Data stored in last
+	 */
 	var peekLast() {
 		if (header != NULL) {
 			return last->data;
@@ -301,23 +386,33 @@ class LinkedList
 	}
 
 	// Metodos Recursivos Definidos en Private de LinkedList
-	// Contains V2
-	/* bool containsV2(var data) { */
-	/* 	return containsRecursivo(data, header); */
-	/* } */
 
-	// Last Index Of
+	/**
+	 * @brief Gets the index where data is stored, from last to header. O(n)
+	 * 
+	 * @param data Data to be found
+	 * @return int - Index of position in LinkedList
+	 */
 	int lastIndexOf(var data) {
 		return lastIndexOfRecursivo(-1, data, last);
 	}
 
-	// Index
+	/**
+	 * @brief Gets the index where data is stored, from header to last. O(n)
+	 * 
+	 * @param data Data to be found
+	 * @return int - Index of position in LinkedList
+	 */
 	int indexOf(var data) {
 		return indexOfRecursivo(0, data, header);
 	}
 
 	// Metodos Adicionales Para Challenge (Reducen la complejidad comparado con el main)
-	// Sum								O(n)
+	/**
+	 * @brief Adds all the data that is stored in the nodes of the LinekdList. O(n)
+	 * 
+	 * @return int - The addition of all the nodes
+	 */
 	int sum() {
 		Node<var>* ptr = this->header;	// O(1)
 		int res = 0;
@@ -328,7 +423,11 @@ class LinkedList
 		return res;
 	}
 
-	// Maximum Node						O(n)
+	/**
+	 * @brief Looks for largest data stored in LinkedList. O(n)
+	 * 
+	 * @return var - Largest data in LinkedList
+	 */
 	var max() {
 		Node<var>* ptr = this->header;	// O(1)
 		var res = ptr->data;
@@ -341,7 +440,11 @@ class LinkedList
 		return res;
 	}
 
-	// Maximum Node						O(n)
+	/**
+	 * @brief Looks for smallest data stored in LinkedList. O(n)
+	 * 
+	 * @return var - Smallest data in LinkedList
+	 */
 	var min() {
 		Node<var>* ptr = this->header;	// O(1)
 		var res = ptr->data;
@@ -355,16 +458,29 @@ class LinkedList
 	}
 
 	// Methods for a Stack
-	// Push
+	/**
+	 * @brief Pushing data into the stack. O(1)
+	 * 
+	 * @param data Data to be pushed
+	 */
 	void push(var data) {
 		addFirst(data);
 	}
 
-	// Pop
+	/**
+	 * @brief Retrieves data of stack and erases it. O(1)
+	 * 
+	 * @return var - Data to be poped
+	 */
 	var pop() {
 		return this->remove_front();
 	}
 
+	/**
+	 * @brief Takes a look at the top of the stack. O(1)
+	 * 
+	 * @return var - Data at the top of stack
+	 */
 	var peak() {
 		if (is_empty()) {
 			return NULL;
@@ -373,12 +489,20 @@ class LinkedList
 	}
 
 	// Methods for a Queue
-	// Queue
+	/**
+	 * @brief Adding a data to the queue. O(1)
+	 * 
+	 * @param data Data to be queued
+	 */
 	void queue(var data) {
 		this-> addLast(data);
 	}
 
-	// Dequeue
+	/**
+	 * @brief Retrieves data and takes it out of the queue. O(1)
+	 * 
+	 * @return var - Data that is dequeued
+	 */
 	var dequeue() {
 		try {                       // O(1)
 			if (header == last) {	// O(1)   Only one data in queue
@@ -503,6 +627,12 @@ class LinkedList
 		}
 	}
 
+	/**
+	 * @brief Checks if data is stored in the LinkedList. O(n)
+	 * 
+	 * @param data Data to be searched in LinkedList
+	 * @return TRUE if data is in LinkedList, else FALSE
+	 */
 	bool checkData(string data)
 	{
 		Node<var>* ptr = header; // Header of a linked list with servers
@@ -522,11 +652,22 @@ class LinkedList
 		return false;
 	}
 
+	/**
+	 * @brief Operator overlad ==, checks if data is within the LinkedList. O(n)
+	 * 
+	 * @param data Data to be checked
+	 * @return TRUE if data is in LinkedList, else FALSE
+	 */
 	bool operator == (string data)
 	{
 		return checkData(data); // Bool statement
 	}
 
+	/**
+	 * @brief Measures the percentage of infected servers within the LinkedList. O(n)
+	 * 
+	 * @return float - Percentage of infected servers
+	 */
 	float infected() {
 		Node<var> * ptr = header; // The current node <server> linked list
 		float total = 0;
@@ -541,10 +682,23 @@ class LinkedList
 		return total/nodeNum;
 	}
 
+	/**
+	 * @brief Gets the percentage of infected in the LinkedList divided by a certain size. O(n)
+	 * 
+	 * @param size - Number of LinkedLists
+	 * @return float - Percentage of infected within all LinkedLists
+	 */
 	float operator / (int size){
 		return infected() / size;
 	}
 
+	/**
+	 * @brief Operator overload <<, used to print the LinkedList. For general purpouse the only node printed is the header. O(1)
+	 * 
+	 * @param salida Out stream variable
+	 * @param list The list object
+	 * @return ostream& 
+	 */
 	friend ostream& operator <<(ostream & salida, const LinkedList & list)
 	{
 		salida << *list.header;
